edcc02f118e76352bb462145857d89a2c6c57900
rubikill <hathietthu2010@gmail.com>
2013-10-14 20:30:34 -0700

    fix

---
+++ FaceBookAPIDemo/java/main/src/controllers/Application.java
@@ -0,0 +1,83 @@
+package controllers;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import managers.FacebookManager;
+import models.Status;
+
+import org.codehaus.jackson.JsonNode;
+
+import facebook4j.Comment;
+import facebook4j.Media;
+import facebook4j.Post;
+
+import play.mvc.Controller;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
+import play.mvc.Result;
+import utils.Global;
+import utils.Tools;
+
+public class Application extends Controller {
+
+	public static Result index() {
+		return ok("Hello");
+	}
+
+	public static Result getFeed() {
+
+		List<models.Status> s = new ArrayList<models.Status>();
+
+		for (Post post : (new FacebookManager()).getFeeds()) {
+			models.Status temp = new models.Status();
+			temp.message = post.getMessage();
+			temp.type = post.getType();
+			temp.url = post.getPicture();
+			temp.name = post.getFrom().getName();
+
+			s.add(temp);
+		}
+
+		return ok(Tools.listToJson(s));
+	}
+
+	public static Result getStatus() {
+		return ok();
+	}
+
+	public static Result postStatus(String s) {
+		System.out.println(s);
+		System.out.println("-------------------------------------");
+		// System.out.println(Global.OAuthAccessToken);
+		try {
+			(new FacebookManager()).postStatus(s);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+
+		System.out.println("OK");
+		return ok("");
+	}
+
+	public static Result postPhoto(String s) {
+		List<FilePart> fileParts = request().body().asMultipartFormData()
+				.getFiles();
+		for (FilePart filePart : fileParts) {
+			(new FacebookManager()).postPhoto(new Media(filePart.getFile()));
+		}
+
+		System.out.println("OK");
+		return ok("OK men!");
+	}
+
+	public static Result setToken(String token) {
+
+		// /System.out.println(token);
+
+		Global.OAuthAccessToken = token;
+		return ok();
+	}
+}
