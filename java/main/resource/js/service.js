e5a7459f27845cffe803de02720237f526a6eb1c
Toan Ha <hathietthu2010@gmail.com>
2014-10-11 11:59:21 +0700

    update client

--- java/main/resource/js/service.js
+++ java/main/resource/js/service.js
@@ -1,57 +1,6 @@
 'use strict';
 
-angular.module('scroll', []).directive('whenScrolled', function() {
-    return function(scope, elm, attr) {
-        var raw = elm[0];
-        
-        elm.bind('scroll', function() {
-            if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
-                scope.$apply(attr.whenScrolled);
-            }
-        });
-    };
-});
-
-var appservice = angular.module('app.services', [ 'ngResource',
-		'angularFileUpload' , 'ui.bootstrap']);
-
-appservice.filter('startFrom', function() {
-	return function(input, start) {
-		if (input) {
-			start = +start; // parse to int
-			return input.slice(start);
-		}
-		return [];
-	}
-});	
-	
-appservice.factory('Home', function($resource) {
-	return $resource('/status/:content', {
-		content : "@content"
-	}, {
-		getStatus : {
-			method : 'GET'
-		},
-		postStatus : {
-			method : 'POST'
-		}
-	});
-});
-
-appservice.factory('Photo', function($resource) {
-	return $resource('/photo/:photo', {
-		photo : "@photo"
-	}, {
-		postPhoto : {
-			method : 'POST'
-		},
-		getPhoto : {
-			method : 'GET'
-		}
-	});
-});
-
-appservice.factory('Auth', function($resource) {
+app.factory('Auth', function($resource) {
 	return $resource('/auth/:token', {
 		token : "@token"
 	}, {
@@ -60,66 +9,7 @@ appservice.factory('Auth', function($resource) {
 		}
 	});
 });
-
-appservice.factory('Feed', function($resource) {
-	return $resource('/feeds/:id/:action', {
-		id : "@id",
-		action : "@action"
-	}, {
-		getFeeds : {
-			method : 'GET',
-			isArray : true
-		},
-		like : {
-			method : 'POST',
-			params : {
-				action : "like"
-			}
-		},
-		unlike : {
-			method : 'POST',
-			params : {
-				action : "unlike"
-			}
-		}
-	});
-});
-
-appservice.factory('Album', function($resource) {
-	return $resource('/album/:id', {
-		id : "@id"
-	}, {
-		getAlbums : {
-			method : 'GET',
-			isArray : true
-		},
-		getAlbum : {
-			method : 'GET',
-			isArray : true
-		}
-	});
-});
-
-appservice.factory('User', function($resource) {
-	return $resource('/user/:id', {
-		id : "@id"
-	}, {
-		getUser : {
-			method : 'GET'
-		}
-	});
-});
-
-appservice.factory('Page', function($resource) {
-	return $resource('/page', {}, {
-		getPagesAdmin : {
-			method : 'GET',
-			isArray : true
-		}
-	});
-});
-
-appservice.factory('Group', function($resource) {
+app.factory('Group', function($resource) {
 	return $resource('/group/:id/:action/:by/:par1/:after/:par2', {
 		id : "@id",
 		action : "@action",
@@ -206,24 +96,3 @@ appservice.factory('Group', function($resource) {
 		}
 	});
 });
-
-
-appservice.factory('RSS', function($resource) {
-	return $resource('/rss/:id/:tableid/:type', {
-		id : "@id",
-		tableid : "@tableid",
-		type : "@type"
-	}, {
-		getRSS : {
-			method : 'GET'
-		},
-		getRSSJson : {
-			method : 'GET',
-			isArray : true,
-			params : {
-				type : "json"
-			}
-		}
-	});
-});
-
